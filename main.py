e94590b0a524ed33b0275e8ad65f23247ce22bb2
jgalvez1776 <jgalvez1776@gmail.com>
2020-06-24 15:20:41 -0700

    Added Morse Translation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-morse_code = {
+LETTER_TO_ROMAN = {
                 'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
                 'E': '.' , 'F': '..-.', 'G': '--.', 'H': '....',
                 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
@@ -11,8 +11,44 @@ morse_code = {
                 '7': '--...', '8': '---..', '9': '----.', '0': '-----',
                 '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
                 ':': '---...',  ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
-                '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+                '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.',
+                ' ': '     '
             }
 
-# TODO: use the dictionary to convert to and from morse code and text
-# Maybe use to make other convertions like a ceasar cypher or other cyphers from cryptography
+MORSE_TO_LETTER = {value: key for (key, value) in LETTER_TO_ROMAN.items()}
+
+
+def convert_to_morse(data):
+    # Maybe find a way to make translation be a list converted to a string?
+    translation = ''
+    for character in data.upper():
+        translation += LETTER_TO_ROMAN[character] + ' '
+    return translation[:-1]
+
+def convert_to_roman(data):
+    counter = 0
+    translation = ''
+    for character in data.split(' '):
+        if character == '':
+            counter += 1
+            if counter == 6:
+                counter = 0
+                translation += ' '
+        else:
+            translation += MORSE_TO_LETTER[character]
+    return translation
+
+
+def main():
+    # Main is to be used in testing. Remove once the program is finished.
+    phrase = 'This here is a test!'
+    phrase2 = convert_to_morse(phrase)
+    phrase3 = convert_to_roman(phrase2)
+    print(phrase)
+    print(phrase2)
+    print(phrase3)
+
+
+if __name__ == '__main__':
+    main()
+
